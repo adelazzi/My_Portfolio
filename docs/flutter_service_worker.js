e6516f3a9781d352a5de8fbeb919d30398116f2b
adelazzi <azziadel3939@gmail.com>
2025-06-22 06:57:54 +0100

    m

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1014,7 +1014,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "92d7cf1459a8e5869b8da28b7cd16780
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "46598ae0017f83283063c3538d5475d0",
+"flutter_bootstrap.js": "710e20789bf3f061f7e6a5417adfe75e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
