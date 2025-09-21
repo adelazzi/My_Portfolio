570a2a884bee65ad7ae798649562a84b976f4f00
ADEL <azziadel3939@gmail.com>
2025-09-21 14:15:30 +0100

    Update accentColor to vivid red in AppColors

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Vivid blue-based gradient colors
   static const Color primaryColor = Color(0xFF0000FF); // Blue
-  static const Color accentColor = Color(0xFF1E90FF); // Dodger Blue
+  static const Color accentColor = Color(0xFFFF0000); // Dodger Blue
   static const Color tertiaryColor = Color(0xFF4169E1); // Royal Blue
   static const Color quaternaryColor = Color(0xFF4682B4); // Steel Blue
 
